f6d1050570ae77dccee0ccf994bcab901685ba3c
carlozanieri <carlo.zanieri@gmail.com>
2025-11-29 00:14:08 +0100

    Versione 6.2

--- casabaldini/settings.py
+++ casabaldini/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    "casabaldini.apps.CasabaldiniConfig",
+   # "casabaldini.apps.CasabaldiniConfig",
     "beb.apps.BebConfig",
     "django.contrib.admin",
     "django.contrib.auth",
